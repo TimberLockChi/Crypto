063aa6f9668310bd5138a70be399f32a3fa9f49e
TimberLockChi <89512364+TimberLockChi@users.noreply.github.com>
2023-09-06 22:06:37 +0800

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Chi Tim on 2023/9/6.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
